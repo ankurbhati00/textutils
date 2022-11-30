f0169a15d7e7f2ca8f08655761cfb9790a2ee498
ankurbhati00 <119473200+ankurbhati00@users.noreply.github.com>
2022-11-30 11:29:01 +0530

    Updates

---
+++ precache-manifest.556271017b230602bb812738ac9f9c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9b266193dd91735ad8a",
+    "url": "/textutils/static/css/main.b92593b4.chunk.css"
+  },
+  {
+    "revision": "b9b266193dd91735ad8a",
+    "url": "/textutils/static/js/main.b9b26619.chunk.js"
+  },
+  {
+    "revision": "271d1efb744527e0afa6",
+    "url": "/textutils/static/js/1.271d1efb.chunk.js"
+  },
+  {
+    "revision": "510fc92f4844e6373fcd",
+    "url": "/textutils/static/js/2.510fc92f.chunk.js"
+  },
+  {
+    "revision": "ecd63b10cc9d2f30fcb5",
+    "url": "/textutils/static/js/runtime~main.ecd63b10.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fe1739604e38aae1eb21cf8c57f34530",
+    "url": "/textutils/index.html"
+  }
+];
